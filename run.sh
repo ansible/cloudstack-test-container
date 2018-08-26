0752a7e3b4346b377b2aac8845a7e0bc7431b4cd
Rene Moser <mail@renemoser.net>
2018-08-26 23:51:16 +0200

    fix simulator not in hypervisor.list

--- run.sh
+++ run.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
+
+# In case the container was restarted
+rm -f /var/www/html/admin.json
+
 until nc -z localhost 8096; do
     echo "waiting for port 8096..."
     sleep 3
 done
 
 sleep 3
-if [ ! -e /var/www/html/admin.json ]
-then
-  export CLOUDSTACK_ENDPOINT=http://127.0.0.1:8096
-  export CLOUDSTACK_KEY=""
-  export CLOUDSTACK_SECRET=""
 
-  # Workaround for Nuage VPC Offering
-  vpc_offering_id="$(cs listVPCOfferings listall=true name=Nuage | jq .vpcoffering[0].id)"
-  cs updateVPCOffering id=$vpc_offering_id state=Disabled
+export CLOUDSTACK_ENDPOINT=http://127.0.0.1:8096
+export CLOUDSTACK_KEY=""
+export CLOUDSTACK_SECRET=""
 
-  admin_id="$(cs listUsers account=admin | jq .user[0].id)"
-  cs getUserKeys id=$admin_id | jq .userkeys > /var/www/html/admin.json
-fi
+admin_id="$(cs listUsers account=admin | jq .user[0].id)"
+cs getUserKeys id=$admin_id | jq .userkeys > /var/www/html/admin.json
